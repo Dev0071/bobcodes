6759abb5d504361eda759449dfb5db31fd4cf341
kev87ian <kelvinian87@gmail.com>
2023-07-31 13:39:01 +0300

    wrote the users' table schema

---
+++ Backend/database/Tables/usersTableSchema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+                       userId NVARCHAR(200),
+                       userName NVARCHAR(200),
+                       email NVARCHAR(200),
+                       password NVARCHAR(500),
+                       isAdmin BIT
+);
