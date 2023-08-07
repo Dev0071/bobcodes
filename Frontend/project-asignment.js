a6ea8ad7073f9778bb3dd589c7b885e4c4e2c563
kev87ian <kelvinian87@gmail.com>
2023-08-07 15:27:19 +0300

    able to fetch project by id om frontend

--- Frontend/project-asignment.js
+++ Frontend/project-asignment.js
@@ -1,14 +1,31 @@
 const urlParams = new URLSearchParams(window.location.search);
 const projectId = urlParams.get('projectId');
-console.log(projectId);
 
-
-async function fetchProjectDetails() = {
+async function fetchProjectDetails() {
     try {
         const response = await fetch(`http://localhost:9500/api/admin/project/${projectId}`, {
             method: "GET"
-        })
+        });
+
+        const projectDetails = await response.json();
+
+        // Select the HTML elements where you want to populate the project details
+        const projectIdElement = document.querySelector('#project-id');
+        const projectName = document.querySelector('#project-name');
+        const projectDescription = document.querySelector('#project-description');
+        const projectEndDate = document.querySelector('#project-end-date');
+        const completionStatus = document.querySelector('.completion-status');
+
+        // Populate the project details into the selected elements
+        projectIdElement.textContent = `Project ID: ${projectDetails.ProjectID}`;
+        projectName.textContent = `Name: ${projectDetails.Name}`;
+        projectDescription.textContent = `Description: ${projectDetails.Description}`;
+        projectEndDate.textContent = `End Date: ${projectDetails.EndDate}`;
+        completionStatus.textContent = `Status: ${projectDetails.IsComplete ? 'Complete' : 'Incomplete'}`;
+
     } catch (error) {
         console.log(error.message);
     }
 }
+
+fetchProjectDetails();
