ae66c10be22e12d427afa818da805b775a113576
kev87ian <kelvinian87@gmail.com>
2023-08-07 16:13:24 +0300

    changes to ui

--- Frontend/project-asignment.js
+++ Frontend/project-asignment.js
@@ -9,18 +9,18 @@ async function fetchProjectDetails() {
 
         const projectDetails = await response.json();
 
-        // Select the HTML elements where you want to populate the project details
+
         const projectIdElement = document.querySelector('#project-id');
         const projectName = document.querySelector('#project-name');
         const projectDescription = document.querySelector('#project-description');
         const projectEndDate = document.querySelector('#project-end-date');
         const completionStatus = document.querySelector('.completion-status');
-
-        // Populate the project details into the selected elements
+        
+        let parsedDate = formatDate(projectDetails.EndDate)
         projectIdElement.textContent = `Project ID: ${projectDetails.ProjectID}`;
         projectName.textContent = `Name: ${projectDetails.Name}`;
         projectDescription.textContent = `Description: ${projectDetails.Description}`;
-        projectEndDate.textContent = `End Date: ${projectDetails.EndDate}`;
+        projectEndDate.textContent = `End Date: ${parsedDate}`;
         completionStatus.textContent = `Status: ${projectDetails.IsComplete ? 'Complete' : 'Incomplete'}`;
 
     } catch (error) {
@@ -28,4 +28,63 @@ async function fetchProjectDetails() {
     }
 }
 
+
+async function fetchUnassignedUsers() {
+    try {
+        const response = await fetch('http://localhost:9500/api/admin/unassigned', {
+            method: "GET"
+        });
+
+        if (response.ok) {
+            const users = await response.json();
+            populateUserList(users);
+        } else {
+            console.log("Something went wrong while fetching users");
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+function populateUserList(users) {
+    const userListContainer = document.getElementById('user-list');
+
+    users.forEach(user => {
+        const userCard = document.createElement('div');
+        userCard.classList.add('user-card');
+
+        const avatar = document.createElement('img');
+        avatar.src =  "/Frontend/images/avatar.png"; 
+        userCard.appendChild(avatar);
+
+        const name = document.createElement('p');
+        name.textContent = `Name: ${user.Username}`;
+        userCard.appendChild(name);
+
+        const email = document.createElement('p');
+        email.textContent = `Email: ${user.Email}`;
+        userCard.appendChild(email);
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        userCard.appendChild(checkbox);
+
+        userListContainer.appendChild(userCard);
+    });
+}
 fetchProjectDetails();
+fetchUnassignedUsers();
+function formatDate(inputDate) {
+
+    const date = new Date(inputDate);
+
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+
+    const paddedDay = day < 10 ? '0' + day : day;
+    const paddedMonth = month < 10 ? '0' + month : month;
+
+    const formattedDate = `${paddedDay}/${paddedMonth}/${year}`;
+
+    return formattedDate;
+}
