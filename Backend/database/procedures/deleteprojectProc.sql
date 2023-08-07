9af1a32c980d51b783f3e43e414e0f26a7f6ba46
Dev0071 <lecturergacheru71@gmail.com>
2023-08-07 09:52:15 +0300

    fixed errors

--- Backend/database/procedures/deleteprojectProc.sql
+++ Backend/database/procedures/deleteprojectProc.sql
@@ -1,18 +1,3 @@
--- CREATE OR ALTER PROCEDURE DeleteProject
---     @ProjectName NVARCHAR(255) 
--- AS
--- BEGIN
-    
---     BEGIN TRANSACTION;
-
---     DELETE FROM UserProjects WHERE ProjectID = (SELECT ProjectID FROM Projects WHERE Name = @ProjectName);
-
---     DELETE FROM Projects WHERE Name = @ProjectName;
-
---     COMMIT TRANSACTION;
-
---     SELECT 'Project deleted successfully' AS Message;
--- END
 
 
 CREATE OR ALTER PROCEDURE DeleteProject
