5af71e4151dafbafb9786408e9ddcc0daeff1fb9
kev87ian <kelvinian87@gmail.com>
2023-08-09 15:26:14 +0300

    assigning a user a project now sends th email

--- Backend/database/Tables/usersTableSchema.sql
+++ Backend/database/Tables/usersTableSchema.sql
@@ -1,12 +1,12 @@
 
--- CREATE TABLE Users (
---     UserID NVARCHAR(255) PRIMARY KEY,
---     Username NVARCHAR(255) NOT NULL,
---     Email NVARCHAR(100) NOT NULL,
---     Password NVARCHAR(500) NOT NULL,
---     IsAssigned BIT NOT NULL DEFAULT 0,
---     isAdmin BIT DEFAULT 0
--- );
+CREATE TABLE Users (
+    UserID NVARCHAR(255) PRIMARY KEY,
+    Username NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(100) NOT NULL,
+    Password NVARCHAR(500) NOT NULL,
+    IsAssigned BIT NOT NULL DEFAULT 0,
+    isAdmin BIT DEFAULT 0
+);
 SELECT * FROM Users
 -- ;
 -- DELETE Users
